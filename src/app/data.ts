d170d35c26a4b2c0cfc566a7852d7c3532b3b151
Patrick McGreevy <patrick7490@icloud.com>
2017-05-17 11:29:08 -0700

    Start data file

---
+++ src/app/data.ts
@@ -0,0 +1,75 @@
+// Purple Line (starting at east terminus)
+export const STOPS: string[] = [
+  'Union Station',
+  'Civic Center/Grand Park',
+  'Pershing Square',
+  '7th St/Metro Center',
+  'Westlake/MacArthur Park',
+  'Wilshire/Vermont',
+  'Wilshire/Normandie',
+  'Wilshire/Western'
+];
+
+// Westbound Purple (Fri/Sat night)
+export const STOP_TIMES: string[][] = [
+  [
+    '12:31',
+    '12:33',
+    '12:34',
+    '12:36',
+    '12:38',
+    '12:40',
+    '12:42',
+    '12:44'
+  ],
+  [
+    '12:51',
+    '12:53',
+    '12:54',
+    '12:56',
+    '12:58',
+    '1:00',
+    '1:02',
+    '1:04'
+  ],
+  [
+    '1:11',
+    '1:13',
+    '1:14',
+    '1:16',
+    '1:18',
+    '1:20',
+    '1:22',
+    '1:24'
+  ],
+  [
+    '1:31',
+    '1:33',
+    '1:34',
+    '1:36',
+    '1:38',
+    '1:40',
+    '1:42',
+    '1:44'
+  ],
+  [
+    '1:51',
+    '1:53',
+    '1:54',
+    '1:56',
+    '1:58',
+    '2:00',
+    '2:02',
+    '2:04'
+  ],
+  [
+    '2:11',
+    '2:13',
+    '2:14',
+    '2:16',
+    '2:18',
+    '2:20',
+    '2:22',
+    '2:24'
+  ]
+];
